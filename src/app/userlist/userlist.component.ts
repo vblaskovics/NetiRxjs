3279fc704f840af89f7b0ba1c33ee8b67427ab16
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-06-30 11:52:33 +0200

    Add async-await solution

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Post } from '../interfaces/post';
 import { User } from '../interfaces/user';
 import { PostService } from '../services/post.service';
 import { UserService } from '../services/user.service';
 
 type Card = {
-  name: string,
-  email: string,
-  postCount: number
-}
+  name: string;
+  email: string;
+  postCount: number;
+};
 
 @Component({
   selector: 'app-userlist',
@@ -24,17 +24,14 @@ export class UserlistComponent implements OnInit {
   users$: Observable<User[]> = new Observable<User[]>();
   posts$: Observable<Post[]> = new Observable<Post[]>();
 
-
   constructor(
     private userService: UserService,
     private postService: PostService
   ) {}
 
   ngOnInit(): void {
-    this.initCardsWithSubscribes();
-
-    // this.users$ = this.userService.getUsers();
-    // this.posts$ = this.postService.getPosts();
+    // this.initCardsWithSubscribes();
+    this.initCardsWithAsyncAwait();
   }
 
   initCardsWithSubscribes() {
@@ -44,14 +41,30 @@ export class UserlistComponent implements OnInit {
         this.posts = posts;
 
         this.users.forEach((u: User) => {
-          let postCount = this.posts.filter((p:Post) => p.userId === u.id).length
+          let postCount = this.posts.filter(
+            (p: Post) => p.userId === u.id
+          ).length;
           this.cards.push({
             name: u.name,
             email: u.email,
-            postCount
-          })
+            postCount,
+          });
         });
-      })
+      });
+    });
+  }
+
+  async initCardsWithAsyncAwait() {
+    this.users = await firstValueFrom(this.userService.getUsers());
+    this.posts = await firstValueFrom(this.postService.getPosts());
+
+    this.users.forEach((u: User) => {
+      let postCount = this.posts.filter((p: Post) => p.userId === u.id).length;
+      this.cards.push({
+        name: u.name,
+        email: u.email,
+        postCount,
+      });
     });
   }
 
